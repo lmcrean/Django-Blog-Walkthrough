503e46c0108c443ec7c78c1ba305c2aae6bf07fa
Laurie C <133490867+lmcrean@users.noreply.github.com>
2023-10-28 22:04:51 +0100

    deployment commit

--- codestar2021/settings.py
+++ codestar2021/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
+if os.path.isfile("env.py"):
+   import env
+
+SECRET_KEY = os.environ.get("mi+^^l3r#w+ani(6knzp@r)bk*q!=m_c+7*-i-r((ygre%-qmy")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,11 +81,15 @@ WSGI_APPLICATION = 'codestar2021.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
